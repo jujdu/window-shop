8ce6819e784aef192f6d2cced26ba14c4e6a6ccc
jujdu <michael.sidoruk@icloud.com>
2019-07-19 19:27:01 +0300

    first commit

---
+++ window-shoper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shoper
+//
+//  Created by Michael Sidoruk on 19/07/2019.
+//  Copyright © 2019 Michael Sidoruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
